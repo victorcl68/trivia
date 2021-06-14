373f6688fc13ff6501625d96c9885ce1e69d692e
Victor Lopes <victor.cabrera0209@gmail.com>
2021-06-14 18:46:09 -0300

    Group-19-req-04

---
+++ src/components/Header.js
@@ -0,0 +1,52 @@
+import md5 from 'crypto-js/md5';
+import { connect } from 'react-redux';
+
+const PropTypes = require('prop-types');
+const React = require('react');
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSrc = this.handleSrc.bind(this);
+  }
+
+  handleSrc() {
+    const { gravatarEmail } = this.props;
+    const hash = md5(gravatarEmail).toString();
+    const gravatarLink = `https://gravatar.com/avatar/${hash}`;
+    return gravatarLink;
+  }
+
+  render() {
+    const { name } = this.props;
+
+    return (
+      <main>
+        <h1>
+          Name:
+          <span data-testid="header-player-name">
+            { name }
+          </span>
+        </h1>
+        <img src={ this.handleSrc() } data-testid="header-profile-picture" alt="" />
+        <h2>
+          Score:
+          <span data-testid="header-score">0</span>
+        </h2>
+      </main>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  name: state.player.name,
+  gravatarEmail: state.player.gravatarEmail,
+});
+
+Header.propTypes = {
+  gravatarEmail: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
