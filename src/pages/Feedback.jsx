7412688ccfca0776c12efb6a791638f1c05a2a3e
Joao Vanelli <vanellijoao@gmail.com>
2021-06-16 11:23:55 -0300

    Requisito 12

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,22 @@
+// import PropTypes from 'prop-types';
+
+import Header from '../components/Header';
+
+const React = require('react');
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <h1>aaa</h1>
+      </>
+    );
+  }
+}
+
+// Feedback.propTypes = {
+//   history: PropTypes.shape().isRequired,
+// };
+
+export default Feedback;
